688d11185d829797f255de4618296b67917d0cea
NguyenLong <rongnhat2@gmail.com>
2022-03-07 05:23:20 +0700

    create statistic

--- resources/views/customer/layout.blade.php
+++ resources/views/customer/layout.blade.php
@@ -183,79 +183,22 @@
                                 <div class="ec-footer-logo"><a href="#"><img src="assets/images/logo/footer-logo.png"
                                             alt=""><img class="dark-footer-logo" src="assets/images/logo/dark-logo.png"
                                             alt="Site Logo" style="display: none;" /></a></div>
-                                <h4 class="ec-footer-heading">Contact us</h4>
+                                <h4 class="ec-footer-heading">Liên hệ</h4>
                                 <div class="ec-footer-links">
                                     <ul class="align-items-center">
-                                        <li class="ec-footer-link">71 Pilgrim Avenue Chevy Chase, east california.</li>
-                                        <li class="ec-footer-link"><span>Call Us:</span><a href="tel:+440123456789">+44
-                                                0123 456 789</a></li>
-                                        <li class="ec-footer-link"><span>Email:</span><a href="/cdn-cgi/l/email-protection#a9ccd1c8c4d9c5cce9ccca84ccc4c8c0c587cac6c4"><span class="__cf_email__" data-cfemail="250e405d4448554940654046084048444c490b464a48">[email&#160;protected]</span></a></li>
+                                        <li class="ec-footer-link"> 124 minh khai, minh khai , hai bà trưng , hn</li>
+                                        <li class="ec-footer-link">0915822772</li> 
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-lg-2 ec-footer-info">
-                            <div class="ec-footer-widget">
-                                <h4 class="ec-footer-heading">Information</h4>
-                                <div class="ec-footer-links">
-                                    <ul class="align-items-center">
-                                        <li class="ec-footer-link"><a href="about-us.html">About us</a></li>
-                                        <li class="ec-footer-link"><a href="faq.html">FAQ</a></li>
-                                        <li class="ec-footer-link"><a href="track-order.html">Delivery Information</a></li>
-                                        <li class="ec-footer-link"><a href="contact-us.html">Contact us</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="col-sm-12 col-lg-2 ec-footer-info"> 
                         </div>
-                        <div class="col-sm-12 col-lg-2 ec-footer-account">
-                            <div class="ec-footer-widget">
-                                <h4 class="ec-footer-heading">Account</h4>
-                                <div class="ec-footer-links">
-                                    <ul class="align-items-center">
-                                        <li class="ec-footer-link"><a href="user-profile.html">My Account</a></li>
-                                        <li class="ec-footer-link"><a href="track-order.html">Order History</a></li>
-                                        <li class="ec-footer-link"><a href="wishlist.html">Wish List</a></li>
-                                        <li class="ec-footer-link"><a href="offer.html">Specials</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="col-sm-12 col-lg-2 ec-footer-account"> 
                         </div>
-                        <div class="col-sm-12 col-lg-2 ec-footer-service">
-                            <div class="ec-footer-widget">
-                                <h4 class="ec-footer-heading">Services</h4>
-                                <div class="ec-footer-links">
-                                    <ul class="align-items-center">
-                                        <li class="ec-footer-link"><a href="track-order.html">Discount Returns</a></li>
-                                        <li class="ec-footer-link"><a href="privacy-policy.html">Policy & policy </a></li>
-                                        <li class="ec-footer-link"><a href="terms-condition.html">Customer Service</a></li>
-                                        <li class="ec-footer-link"><a href="terms-condition.html">Term & condition</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="col-sm-12 col-lg-2 ec-footer-service"> 
                         </div>
-                        <div class="col-sm-12 col-lg-3 ec-footer-news">
-                            <div class="ec-footer-widget">
-                                <h4 class="ec-footer-heading">Newsletter</h4>
-                                <div class="ec-footer-links">
-                                    <ul class="align-items-center">
-                                        <li class="ec-footer-link">Get instant updates about our new products and
-                                            special promos!</li>
-                                    </ul>
-                                    <div class="ec-subscribe-form">
-                                        <form id="ec-newsletter-form" name="ec-newsletter-form" method="post"
-                                            action="#">
-                                            <div id="ec_news_signup" class="ec-form">
-                                                <input class="ec-email" type="email" required=""
-                                                    placeholder="Enter your email here..." name="ec-email" value="" />
-                                                <button id="ec-news-btn" class="button btn-primary" type="submit"
-                                                    name="subscribe" value=""><i class="ecicon eci-paper-plane-o"
-                                                        aria-hidden="true"></i></button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="col-sm-12 col-lg-3 ec-footer-news"> 
                         </div>
                     </div>
                 </div>
@@ -266,7 +209,7 @@
                         <!-- Footer social Start -->
                         <div class="col text-left footer-bottom-left">
                             <div class="footer-bottom-social">
-                                <span class="social-text text-upper">Follow us on:</span>
+                                <span class="social-text text-upper">Theo dõi chúng tôi:</span>
                                 <ul class="mb-0">
                                     <li class="list-inline-item"><a class="hdr-facebook" href="#"><i class="ecicon eci-facebook"></i></a></li>
                                     <li class="list-inline-item"><a class="hdr-twitter" href="#"><i class="ecicon eci-twitter"></i></a></li>
@@ -280,7 +223,7 @@
                         <div class="col text-center footer-copy">
                             <div class="footer-bottom-copy ">
                                 <div class="ec-copy">Copyright © 2021-2022 <a class="site-name text-upper"
-                                        href="#">ekka<span>.</span></a>. All Rights Reserved</div>
+                                        href="#">Vu Khanh Ha<span>.</span></a>. All Rights Reserved</div>
                             </div>
                         </div>
                         <!-- Footer Copyright End -->
